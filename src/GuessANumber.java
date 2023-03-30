6f24a7f6e8a7604b3a5f88e9a1f3c3cd53291800
Stavromir <s.kasapov@protonmail.com>
2023-03-30 18:56:23 +0300

    name change

--- src/GuessANumber.java
+++ src/GuessANumber.java
@@ -1,6 +1,8 @@
+package src;
+
 import java.util.Scanner;
 
-public class Main {
+public class GuessANumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
