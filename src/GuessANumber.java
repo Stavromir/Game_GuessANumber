41228b16441c2cd6602bcb9acbcbb22697e9b13d
Stavromir <s.kasapov@protonmail.com>
2023-03-30 19:00:18 +0300

    add computer move

--- src/GuessANumber.java
+++ src/GuessANumber.java
@@ -1,10 +1,16 @@
 package src;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class GuessANumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        Random randomNumber = new Random();
+        int computerMove = randomNumber.nextInt(100);
+
+
 
 
     }
