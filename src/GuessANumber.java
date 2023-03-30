28b2d6083af1c1ad1c9c48bd640fc240c31f3764
Stavromir <s.kasapov@protonmail.com>
2023-03-30 23:13:20 +0300

    wrong input case, code optimization

--- src/GuessANumber.java
+++ src/GuessANumber.java
@@ -8,25 +8,41 @@ public class GuessANumber {
         Scanner scanner = new Scanner(System.in);
 
         Random randomNumber = new Random();
-        int computerMove = randomNumber.nextInt(100);
+        int computerMove = randomNumber.nextInt(101);
 
-        System.out.print("Guess a number from 1 to 101: ");
+        System.out.print("Guess a number from 1 to 100: ");
 
-        int playerMove = Integer.parseInt(scanner.nextLine());
+        String playerInput = scanner.nextLine();
 
-        while (computerMove != playerMove) {
+        while (true) {
 
-            if (computerMove > playerMove) {
-                System.out.print("Too low! Please try again: ");
-            } else {
-                System.out.print("Too high! Please try again: ");
+            boolean wrongInput = false;
+
+            for (int i = 0; i < playerInput.length(); i++) {
+                if (!Character.isDigit(playerInput.charAt(i))) {
+                    System.out.print("Wrong input! Please try again: ");
+                    wrongInput = true;
+                }
             }
 
-            playerMove = Integer.parseInt(scanner.nextLine());
+            if (wrongInput) {
+                playerInput = scanner.nextLine();
+                continue;
+            }
 
+            int playerNumber = Integer.parseInt(playerInput);
+
+
+            if (computerMove > playerNumber) {
+                System.out.print("Too low! Please try again: ");
+                playerInput = scanner.nextLine();
+            } else if (computerMove < playerNumber) {
+                System.out.print("Too high! Please try again: ");
+                playerInput = scanner.nextLine();
+            } else {
+                System.out.println("You guessed it!");
+                break;
+            }
         }
-
-            System.out.println("You guessed it!");
-
     }
 }
