7f14ae97a47a876f5924ff8571a169951853285f
Stavromir <s.kasapov@protonmail.com>
2023-04-02 12:07:32 +0300

    add new game option, code optimization

--- src/GuessANumber.java
+++ src/GuessANumber.java
@@ -22,6 +22,7 @@ public class GuessANumber {
                 if (!Character.isDigit(playerInput.charAt(i))) {
                     System.out.print("Wrong input! Please try again: ");
                     wrongInput = true;
+                    break;
                 }
             }
 
@@ -36,11 +37,39 @@ public class GuessANumber {
             if (computerMove > playerNumber) {
                 System.out.print("Too low! Please try again: ");
                 playerInput = scanner.nextLine();
+                continue;
             } else if (computerMove < playerNumber) {
                 System.out.print("Too high! Please try again: ");
                 playerInput = scanner.nextLine();
+                continue;
             } else {
                 System.out.println("You guessed it!");
+                System.out.println();
+                System.out.println("Do you want to play again?");
+                System.out.print("[Y]es or [N]o: ");
+            }
+
+            boolean endGame = false;
+
+            while (true) {
+
+                String newGame = scanner.nextLine();
+
+                if (newGame.equals("Y") || newGame.equals("Yes") || newGame.equals("y")){
+                    computerMove = randomNumber.nextInt(101);
+                    System.out.print("Guess a number from 1 to 100: ");
+                    playerInput = scanner.nextLine();
+                    break;
+                } else if (newGame.equals("N") || newGame.equals("No") || newGame.equals("n")) {
+                    System.out.println("Good bye!");
+                    endGame = true;
+                    break;
+                } else {
+                    System.out.println("Wrong input! Please try again.");
+                    System.out.print("[Y]es or [N]o: ");
+                }
+            }
+            if (endGame) {
                 break;
             }
         }
